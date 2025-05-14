df70c0ba30f1931e8df8c1545364703eebe7e468
assassinsman123 <135070306+assassinsman123@users.noreply.github.com>
2025-05-15 11:33:24 +1200

    finished main menu coding the level select

---
+++ 301.3 MVP Tower Defence Game/Assets/C# codes/LevelLoader.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class LevelLoader : MonoBehaviour
+{
+    public void LoadLevel()
+   
+}
--- 301.3 MVP Tower Defence Game/Assets/C# codes/Mainmenu.cs
+++ 301.3 MVP Tower Defence Game/Assets/C# codes/Mainmenu.cs
@@ -6,5 +6,13 @@ public class Mainmenu : MonoBehaviour
     public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        Debug.Log("PLAY");
     }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("QUIT");
+    }
+
 }
