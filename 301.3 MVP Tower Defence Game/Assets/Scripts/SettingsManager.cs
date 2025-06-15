c35820a445d1254d963f8bc6de05b3e9165b0655
assassinsman123 <135070306+assassinsman123@users.noreply.github.com>
2025-06-15 20:00:34 +1200

    focusing on getting the pop text to work for towers
    working on getting text to pop up when clicking on the towers,upgrades

--- 301.3 MVP Tower Defence Game/Assets/Scripts/SettingsManager.cs
+++ 301.3 MVP Tower Defence Game/Assets/Scripts/SettingsManager.cs
@@ -1,48 +1,57 @@
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class SettingsManager : MonoBehaviour
 {
     public GameObject SettingsPanel;
+    public GameObject OptionsPanel; // 👈 Add this in the Inspector
     public Button soundButton;
+
     private bool isSoundOn = true;
+
     void Start()
     {
         SettingsPanel.SetActive(false);
+        if (OptionsPanel != null) OptionsPanel.SetActive(false);
+
         soundButton.onClick.AddListener(ToggleSound);
     }
 
     public void OpenSettings()
-    { 
-     SettingsPanel.SetActive(true);
+    {
+        SettingsPanel.SetActive(true);
+        if (OptionsPanel != null) OptionsPanel.SetActive(false);
     }
 
     public void CloseSettings()
     {
         SettingsPanel.SetActive(false);
+        if (OptionsPanel != null) OptionsPanel.SetActive(false);
     }
 
     public void GoHome()
-    {        
-        SceneManager.LoadScene("Main menu scene");
+    {
+        SceneManager.LoadScene("Main menu scene"); // Replace with your actual scene name
     }
 
     void ToggleSound()
-    { 
-     isSoundOn = !isSoundOn;
+    {
+        isSoundOn = !isSoundOn;
         AudioListener.volume = isSoundOn ? 1 : 0;
         soundButton.GetComponentInChildren<Text>().text = isSoundOn ? "Sound: ON" : "Sound: OFF";
     }
 
     public void ToggleSettings()
     {
-        SettingsPanel.SetActive(!SettingsPanel.activeSelf);
+        bool show = !SettingsPanel.activeSelf;
+        SettingsPanel.SetActive(show);
+        if (OptionsPanel != null) OptionsPanel.SetActive(false);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void GoToOptions()
     {
-        
+        if (SettingsPanel != null) SettingsPanel.SetActive(false);
+        if (OptionsPanel != null) OptionsPanel.SetActive(true);
     }
 }
